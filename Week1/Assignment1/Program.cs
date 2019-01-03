e4744121bc7f69d4b4d25f9b46881eb1847f9635
Yuan Wang <wang17@ualberta.ca>
2019-01-02 19:36:19 -0700

    add week1

---
+++ Week1/Assignment1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first X value: ");
+            float point1X = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter first Y value: ");
+            float point1Y = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter second X value: ");
+            float point2X = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter second Y value: ");
+            float point2Y = float.Parse(Console.ReadLine());
+
+            double deltaY = point2Y - point1Y;
+            double deltaX = point2X - point1X;
+
+            double distance = Math.Sqrt(Math.Pow(deltaX, 2)+Math.Pow(deltaY,2));
+
+            Console.WriteLine("Distance between points:" + distance);
+
+            double angle = Math.Atan2(deltaX, deltaY) * (180 / Math.PI);
+            Console.WriteLine("Angle between points: "+angle+" degrees");
+
+        }
+    }
+}
